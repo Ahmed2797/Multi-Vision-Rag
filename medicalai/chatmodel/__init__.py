f65a033a37fdaa54e6c092b4b185495b35682f84
Ahmed <tanvirahmed754575@gmail.com>
2026-02-22 23:47:43 +0600

    chat model

--- medicalai/chatmodel/__init__.py
+++ medicalai/chatmodel/__init__.py
@@ -5,15 +5,17 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+if OPENAI_API_KEY:
+    os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 def openai_chat_model(model_name:str="gpt-4.1-nano"):
     
     chat_model = ChatOpenAI(
     model= model_name, ## "gpt-4.1-nano", ## "gpt-4o-mini",  ## "gpt-5-nano",  # OpenAI chat model
     temperature=0.4, 
-    max_tokens=500
+    max_tokens=500,
+    api_key=OPENAI_API_KEY,
     )
     
     return chat_model
@@ -24,4 +26,3 @@ def openai_embedding(model_name:str='text-embedding-3-small'):
         api_key=OPENAI_API_KEY
         )
     return embeddings
-
